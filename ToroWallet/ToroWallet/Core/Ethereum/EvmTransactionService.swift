9e3777c7e4b6c87203831387654a36771e974114
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:47:33 +0600

    Update EvmTransactionService.swift

--- ToroWallet/ToroWallet/Core/Ethereum/EvmTransactionService.swift
+++ ToroWallet/ToroWallet/Core/Ethereum/EvmTransactionService.swift
@@ -68,3 +68,132 @@ class EvmTransactionService {
             }
         }
     }
+
+    private func gasLimitSingle(gasPrice: Int, transactionData: TransactionData) -> Single<Int> {
+        evmKit.estimateGas(transactionData: transactionData, gasPrice: gasPrice)
+    }
+
+    private func sync() {
+        disposeBag = DisposeBag()
+
+        guard let transactionData = transactionData else {
+            transactionStatus = .failed(GasDataError.noTransactionData)
+            return
+        }
+
+        transactionStatus = .loading
+
+        gasPriceSingle(gasPriceType: gasPriceType)
+                .flatMap { [unowned self] gasPrice -> Single<Transaction> in
+                    transactionSingle(gasPrice: gasPrice, transactionData: transactionData)
+                }
+                .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+                .subscribe(onSuccess: { [weak self] transaction in
+                    self?.transactionStatus = .completed(transaction)
+                }, onError: { [weak self] error in
+                     self?.transactionStatus = .failed(error)
+                })
+                .disposed(by: disposeBag)
+    }
+
+    private var evmBalance: BigUInt {
+        evmKit.accountState?.balance ?? 0
+    }
+
+    private func surchargedGasLimit(estimatedGasLimit: Int) -> Int {
+        estimatedGasLimit + Int(Double(estimatedGasLimit) / 100.0 * Double(gasLimitSurchargePercent))
+    }
+
+    private func transactionSingle(gasPrice: Int, transactionData: TransactionData) -> Single<Transaction> {
+        adjustedTransactionDataSingle(gasPrice: gasPrice, transactionData: transactionData).flatMap { [unowned self] transactionData in
+            gasLimitSingle(gasPrice: gasPrice, transactionData: transactionData).map { [unowned self] estimatedGasLimit in
+                let gasLimit = surchargedGasLimit(estimatedGasLimit: estimatedGasLimit)
+
+                return Transaction(
+                        transactionData: transactionData,
+                        gasData: GasData(estimatedGasLimit: estimatedGasLimit, gasLimit: gasLimit, gasPrice: gasPrice)
+                )
+            }
+        }
+    }
+
+    private func adjustedTransactionDataSingle(gasPrice: Int, transactionData: TransactionData) -> Single<TransactionData> {
+        if transactionData.input.isEmpty && transactionData.value == evmBalance {
+            let stubTransactionData = TransactionData(to: transactionData.to, value: 1, input: Data())
+
+            return gasLimitSingle(gasPrice: gasPrice, transactionData: stubTransactionData).flatMap { [unowned self] estimatedGasLimit in
+                let gasLimit = surchargedGasLimit(estimatedGasLimit: estimatedGasLimit)
+                let adjustedValue = transactionData.value - BigUInt(gasLimit) * BigUInt(gasPrice)
+
+                if adjustedValue <= 0 {
+                    return Single.error(GasDataError.insufficientBalance)
+                } else {
+                    let adjustedTransactionData = TransactionData(to: transactionData.to, value: adjustedValue, input: Data())
+                    return Single.just(adjustedTransactionData)
+                }
+            }
+        } else {
+            return Single.just(transactionData)
+        }
+    }
+
+}
+
+extension EvmTransactionService: IEvmTransactionFeeService {
+
+    var customFeeRange: ClosedRange<Int> {
+        feeRateProvider.customFeeRange
+    }
+
+    var hasEstimatedFee: Bool {
+        gasLimitSurchargePercent != 0
+    }
+
+    var gasPriceTypeObservable: Observable<GasPriceType> {
+        gasPriceTypeRelay.asObservable()
+    }
+
+    var warningOfStuckObservable: Observable<Bool> {
+        warningOfStuckRelay.asObservable()
+    }
+
+    var transactionStatusObservable: Observable<DataStatus<Transaction>> {
+        transactionStatusRelay.asObservable()
+    }
+
+    func set(transactionData: TransactionData?) {
+        self.transactionData = transactionData
+        sync()
+    }
+
+    func set(gasPriceType: GasPriceType) {
+        self.gasPriceType = gasPriceType
+        sync()
+    }
+
+}
+
+extension EvmTransactionService {
+
+    struct GasData {
+        let estimatedGasLimit: Int
+        let gasLimit: Int
+        let gasPrice: Int
+
+        var estimatedFee: BigUInt {
+            BigUInt(estimatedGasLimit * gasPrice)
+        }
+
+        var fee: BigUInt {
+            BigUInt(gasLimit * gasPrice)
+        }
+    }
+
+    struct Transaction {
+        let transactionData: TransactionData
+        let gasData: GasData
+
+        var totalAmount: BigUInt {
+            transactionData.value + gasData.fee
+        }
+    }
