82df3a3bb67bf6231238559ccb2ed82be75e916b
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:37:23 +0600

    Update FiatService.swift

--- ToroWallet/ToroWallet/Core/Fiat/FiatService.swift
+++ ToroWallet/ToroWallet/Core/Fiat/FiatService.swift
@@ -2,3 +2,19 @@ import CurrencyKit
 import RxSwift
 import RxRelay
 import MarketKit
+
+class FiatService {
+    private var disposeBag = DisposeBag()
+    private var coinPriceDisposeBag = DisposeBag()
+    private var queue = DispatchQueue(label: "io.horizontalsystems.unstoppable.fiat-service", qos: .userInitiated)
+
+    private let switchService: AmountTypeSwitchService
+    private let currencyKit: CurrencyKit.Kit
+    private let marketKit: MarketKit.Kit
+
+    private var platformCoin: PlatformCoin?
+    private var price: Decimal? {
+        didSet {
+            toggleAvailableRelay.accept(price != nil)
+        }
+    }
