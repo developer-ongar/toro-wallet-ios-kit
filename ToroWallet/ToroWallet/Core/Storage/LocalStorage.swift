7c534073cec178fbf38414baeedfe90f5ca13590
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:49:52 +0600

    Create LocalStorage.swift

---
+++ ToroWallet/ToroWallet/Core/Storage/LocalStorage.swift
@@ -0,0 +1,27 @@
+import Foundation
+import StorageKit
+import MarketKit
+
+class LocalStorage {
+    private let agreementAcceptedKey = "i_understand_key"
+    private let biometricOnKey = "biometric_on_key"
+    private let lastExitDateKey = "last_exit_date_key"
+    private let keySendInputType = "amount-type-switch-service-amount-type"
+    private let keyChartType = "chart_type_key"
+    private let mainShownOnceKey = "main_shown_once_key"
+    private let jailbreakShownOnceKey = "jailbreak_shown_once_key"
+    private let debugLogKey = "debug_log_key"
+    private let keyTransactionDataSortMode = "transaction_data_sort_mode"
+    private let keyLockTimeEnabled = "lock_time_enabled"
+    private let keyAppLaunchCount = "app_launch_count"
+    private let keyRateAppLastRequestDate = "rate_app_last_request_date"
+    private let keyZCashRewind = "z_cash_always_pending_rewind"
+    private let keyDefaultProvider = "swap_provider"
+
+    private let storage: StorageKit.ILocalStorage
+
+    init(storage: StorageKit.ILocalStorage) {
+        self.storage = storage
+    }
+
+}
