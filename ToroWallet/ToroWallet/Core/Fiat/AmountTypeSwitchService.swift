e56e19b0ff72481500b0b11391d44939ceddf267
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:39:54 +0600

    Update AmountTypeSwitchService.swift

--- ToroWallet/ToroWallet/Core/Fiat/AmountTypeSwitchService.swift
+++ ToroWallet/ToroWallet/Core/Fiat/AmountTypeSwitchService.swift
@@ -1,3 +1,26 @@
 import RxSwift
 import RxRelay
 import StorageKit
+
+class AmountTypeSwitchService {
+    private let amountTypeKey = "amount-type-switch-service-amount-type"
+    private let localStorage: StorageKit.ILocalStorage
+
+    private var toggleAvailableObservables = [Observable<Bool>]()
+    private var disposeBag = DisposeBag()
+
+    private let amountTypeRelay = PublishRelay<AmountType>()
+    private(set) var amountType: AmountType {
+        didSet {
+            amountTypeRelay.accept(amountType)
+        }
+    }
+
+    private var toggleAvailableRelay = PublishRelay<Bool>()
+    private(set) var toggleAvailable: Bool = false {
+        didSet {
+            if toggleAvailable != oldValue {
+                toggleAvailableRelay.accept(toggleAvailable)
+            }
+        }
+    }
