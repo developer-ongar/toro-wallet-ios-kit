5d4d277cb21e065306ccbdfb67c69bc1ad609d76
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:36:13 +0600

    Update RestoreFavoriteCoinWorker.swift

--- ToroWallet/ToroWallet/Core/Workers/RestoreFavoriteCoinWorker.swift
+++ ToroWallet/ToroWallet/Core/Workers/RestoreFavoriteCoinWorker.swift
@@ -17,3 +17,46 @@ class RestoreFavoriteCoinWorker {
     }
 
 }
+
+extension RestoreFavoriteCoinWorker {
+
+    func run() throws {
+        let alreadyRun: Bool = localStorage.value(for: localStorageKey) ?? false
+
+        guard !alreadyRun else {
+            return
+        }
+
+        localStorage.set(value: true, for: localStorageKey)
+
+        let oldRecords = storage.favoriteCoinRecords_v_0_22
+        let oldCoinTypes = oldRecords.map { $0.coinType }
+
+        var coinsUids = Set<String>()
+
+        for oldCoinType in oldCoinTypes {
+            switch oldCoinType {
+            case .bitcoin: coinsUids.insert("bitcoin")
+            case .bitcoinCash: coinsUids.insert("bitcoin-cash")
+            case .litecoin: coinsUids.insert("litecoin")
+            case .dash: coinsUids.insert("dash")
+            case .zcash: coinsUids.insert("zcash")
+            case .ethereum: coinsUids.insert("ethereum")
+            case .binanceSmartChain: coinsUids.insert("binancecoin")
+            case .erc20, .bep20, .bep2:
+                if let platformCoin = try coinManager.platformCoin(coinType: oldCoinType) {
+                    coinsUids.insert(platformCoin.coin.uid)
+                }
+            case .unsupported(let type):
+                if let fullCoin = try coinManager.fullCoin(coinUid: type) {
+                    coinsUids.insert(fullCoin.coin.uid)
+                }
+            default: ()
+            }
+        }
+
+        favoritesManager.add(coinUids: Array(coinsUids))
+    }
+
+}
+
