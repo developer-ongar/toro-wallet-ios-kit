c91bcf913441faaa71efc4ba0f4169913b147b77
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:29:32 +0600

    Update MarketKitExtensions.swift

--- ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
+++ ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
@@ -13,3 +13,22 @@ extension MarketKit.CoinType {
 
         return nil
     }
+
+    var platformType: String {
+        switch self {
+        case .ethereum, .erc20: return "Ethereum"
+        case .binanceSmartChain, .bep20: return "Binance Smart Chain"
+        case .bep2: return "Binance"
+        default: return ""
+        }
+    }
+
+    var platformCoinType: String {
+        switch self {
+        case .ethereum, .binanceSmartChain: return "coin_platforms.native".localized
+        case .erc20: return "ERC20"
+        case .bep20: return "BEP20"
+        case .bep2: return "BEP2"
+        default: return ""
+        }
+    }
