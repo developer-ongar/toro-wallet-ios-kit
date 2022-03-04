1360347e3792ee3bc412af876374c63a72deaa71
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:35:55 +0600

    Update RestoreFavoriteCoinWorker.swift

--- ToroWallet/ToroWallet/Core/Workers/RestoreFavoriteCoinWorker.swift
+++ ToroWallet/ToroWallet/Core/Workers/RestoreFavoriteCoinWorker.swift
@@ -1,2 +1,19 @@
 import MarketKit
 import StorageKit
+
+class RestoreFavoriteCoinWorker {
+    private let localStorageKey = "restore-favorite-coin-worker-run"
+
+    private let coinManager: CoinManager
+    private let favoritesManager: FavoritesManager
+    private let localStorage: StorageKit.ILocalStorage
+    private let storage: IFavoriteCoinRecordStorage
+
+    init(coinManager: CoinManager, favoritesManager: FavoritesManager, localStorage: StorageKit.ILocalStorage, storage: IFavoriteCoinRecordStorage) {
+        self.coinManager = coinManager
+        self.favoritesManager = favoritesManager
+        self.localStorage = localStorage
+        self.storage = storage
+    }
+
+}
