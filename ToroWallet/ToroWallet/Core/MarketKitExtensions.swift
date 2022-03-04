71daefde6bf44941b0f8636f7618673a2fbc41a7
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:29:50 +0600

    Update MarketKitExtensions.swift

--- ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
+++ ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
@@ -32,3 +32,45 @@ extension MarketKit.CoinType {
         default: return ""
         }
     }
+
+    var swappable: Bool {
+        switch self {
+        case .ethereum, .erc20, .binanceSmartChain, .bep20: return true
+        default: return false
+        }
+    }
+
+    var restoreUrl: String {
+        switch self {
+        case .bitcoin: return "https://btc.horizontalsystems.xyz/apg"
+        case .litecoin: return "https://ltc.horizontalsystems.xyz/api"
+        case .bitcoinCash: return "https://explorer.bitcoin.com/bch/"
+        case .dash: return "https://dash.horizontalsystems.xyz"
+        default: return ""
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .bitcoin: return "Bitcoin"
+        case .litecoin: return "Litecoin"
+        case .bitcoinCash: return "Bitcoin Cash"
+        default: return ""
+        }
+    }
+
+    var coinSettingTypes: [CoinSettingType] {
+        switch self {
+        case .bitcoin, .litecoin: return [.derivation]
+        case .bitcoinCash: return [.bitcoinCashCoinType]
+        default: return []
+        }
+    }
+
+    var defaultSettingsArray: [CoinSettings] {
+        switch self {
+        case .bitcoin, .litecoin: return [[.derivation: MnemonicDerivation.bip49.rawValue]]
+        case .bitcoinCash: return [[.bitcoinCashCoinType: BitcoinCashCoinType.type145.rawValue]]
+        default: return []
+        }
+    }
