f01121e636dfb90971be8bddbea864e4cfdb26e4
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:33:36 +0600

    Update RestoreCustomTokenWorker.swift

--- ToroWallet/ToroWallet/Core/Workers/RestoreCustomTokenWorker.swift
+++ ToroWallet/ToroWallet/Core/Workers/RestoreCustomTokenWorker.swift
@@ -2,3 +2,21 @@ import RxSwift
 import HsToolKit
 import MarketKit
 import StorageKit
+
+class RestoreCustomTokenWorker {
+    private let localStorageKey = "restore-custom-token-worker-run"
+
+    private let coinManager: CoinManager
+    private let walletManager: WalletManager
+    private let storage: IEnabledWalletStorage
+    private let localStorage: StorageKit.ILocalStorage
+    private let networkManager: NetworkManager
+    private let disposeBag = DisposeBag()
+
+    init(coinManager: CoinManager, walletManager: WalletManager, storage: IEnabledWalletStorage, localStorage: StorageKit.ILocalStorage, networkManager: NetworkManager) {
+        self.coinManager = coinManager
+        self.walletManager = walletManager
+        self.storage = storage
+        self.localStorage = localStorage
+        self.networkManager = networkManager
+    }
