7e83ed3ab7fcdd6207a0fc30f623f8af21230c80
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:28:06 +0600

    Update Global.swift

--- ToroWallet/ToroWallet/Core/Global.swift
+++ ToroWallet/ToroWallet/Core/Global.swift
@@ -1,2 +1,18 @@
 import RxSwift
 import RxCocoa
+
+func subscribe<T>(_ disposeBag: DisposeBag, _ driver: Driver<T>, _ onNext: ((T) -> Void)? = nil) {
+    driver.drive(onNext: onNext).disposed(by: disposeBag)
+}
+
+func subscribe<T>(_ disposeBag: DisposeBag, _ signal: Signal<T>, _ onNext: ((T) -> Void)? = nil) {
+    signal.emit(onNext: onNext).disposed(by: disposeBag)
+}
+
+func subscribe<T>(_ disposeBag: DisposeBag, _ observable: Observable<T>, _ onNext: ((T) -> Void)? = nil) {
+    observable
+            .subscribeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+            .observeOn(ConcurrentDispatchQueueScheduler(qos: .userInitiated))
+            .subscribe(onNext: onNext)
+            .disposed(by: disposeBag)
+}
