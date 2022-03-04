91e15fb601295eb4648c7c9b5897f87472c8b516
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:21:54 +0600

    Update Protocols.swift

--- ToroWallet/ToroWallet/Core/Protocols.swift
+++ ToroWallet/ToroWallet/Core/Protocols.swift
@@ -14,3 +14,19 @@ typealias CoinCode = String
 protocol IRandomManager {
     func getRandomIndexes(max: Int, count: Int) -> [Int]
 }
+
+protocol ILocalStorage: AnyObject {
+    var agreementAccepted: Bool { get set }
+    var debugLog: String? { get set }
+    var sendInputType: SendInputType? { get set }
+    var mainShownOnce: Bool { get set }
+    var jailbreakShownOnce: Bool { get set }
+    var transactionDataSortMode: TransactionDataSortMode? { get set }
+    var lockTimeEnabled: Bool { get set }
+    var appLaunchCount: Int { get set }
+    var rateAppLastRequestDate: Date? { get set }
+    var zcashAlwaysPendingRewind: Bool { get set }
+
+    func defaultProvider(blockchain: SwapModule.Dex.Blockchain) -> SwapModule.Dex.Provider
+    func setDefaultProvider(blockchain: SwapModule.Dex.Blockchain, provider: SwapModule.Dex.Provider)
+}
