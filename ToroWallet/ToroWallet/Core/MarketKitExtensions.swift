d593aa7690671397699453e3a348404d41818c8d
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-05 01:30:27 +0600

    Update MarketKitExtensions.swift

--- ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
+++ ToroWallet/ToroWallet/Core/MarketKitExtensions.swift
@@ -176,3 +176,28 @@ extension Array where Element == FullCoin {
             if lhsEnabled != rhsEnabled {
                 return lhsEnabled
             }
+
+              if !filter.isEmpty {
+                let filter = filter.lowercased()
+
+                let lhsExactCode = lhsFullCoin.coin.code.lowercased() == filter
+                let rhsExactCode = rhsFullCoin.coin.code.lowercased() == filter
+
+                if lhsExactCode != rhsExactCode {
+                    return lhsExactCode
+                }
+
+                let lhsStartsWithCode = lhsFullCoin.coin.code.lowercased().starts(with: filter)
+                let rhsStartsWithCode = rhsFullCoin.coin.code.lowercased().starts(with: filter)
+
+                if lhsStartsWithCode != rhsStartsWithCode {
+                    return lhsStartsWithCode
+                }
+
+                let lhsStartsWithName = lhsFullCoin.coin.name.lowercased().starts(with: filter)
+                let rhsStartsWithName = rhsFullCoin.coin.name.lowercased().starts(with: filter)
+
+                if lhsStartsWithName != rhsStartsWithName {
+                    return lhsStartsWithName
+                }
+            }
